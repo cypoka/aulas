2f822f11aba507028c00cf617de3ed75b3467764
cypoka <67935274+cypoka@users.noreply.github.com>
2020-07-06 22:24:16 -0300

    Create aula1.sql

---
+++ aula1.sql
@@ -0,0 +1,23 @@
+create database cadastro;
+create table pessoas (
+nome varchar (30) not null,
+nascimento date,
+sexo enum ('M','F'),
+peso decimal (3,2),
+altura decimal (5,2),
+nacionalidade varchar (20) default 'Brasil'
+);
+
+describe pessoas;
+
+alter table cadastro.pessoas add column `id` int(10) unsigned primary KEY AUTO_INCREMENT;
+
+alter table cadastro.pessoas add column `newid` int(10) unsigned primary KEY AUTO_INCREMENT;
+
+alter table pessoas DROP PRIMARY KEY;
+
+ALTER TABLE pessoas MODIFY id INT NOT NULL;
+
+ALTER TABLE pessoas DROP PRIMARY KEY;
+
+ALTER TABLE pessoas MODIFY id INT NOT NULL primary KEY AUTO_INCREMENT;
